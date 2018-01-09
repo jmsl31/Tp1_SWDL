cdc00d3727d3f02fee27ee1822cd767ff4ab3e99
Etudiant M2 ICE - 2017 - JOHANN MEISSL <johann.meissl@mathinfo.i-univ-tlse2.fr>
2018-01-09 11:14:38 +0100

    Validation du Tp1

--- src/java/applicationclientwsdl/Main.java
+++ src/java/applicationclientwsdl/Main.java
@@ -16,6 +16,18 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        PrevisionMeteoService service = new PrevisionMeteoService();
+        
+        PrevisionMeteoPortType ports = service.getPrevisionMeteoPort();
+        
+        String ville = ports.meteoDemain("Paris");
+        Detail ville2 = ports.meteoDetaillee("Toulouse");
+        
+        System.out.println("Meteo de demain à Paris " +  ville);
+        
+        System.out.println("Meteo de demain à Toulouse " +  ville2.getResume());
+        System.out.println("Temperature de demain à Toulouse " +  ville2.getTemperature());
     }
     
 }
